ccc61249b2ee7e76439218fdb36b3d6881361972
Ali Atakan <ali@cimri.com>
2019-05-20 21:52:04 +0100

    Add GET all students endpoint that retrieve the data from MongoDB, Update README.md, Fix data.json typo

---
+++ routes/students.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+mongoose.connect('mongodb://localhost/mongo-exercise', { useNewUrlParser: true })
+    .then(() => console.log('Mongo connection is OK!'))
+    .catch(err => console.log('Mongo connection is FAILED', err));
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    surname: String,
+    courses: [String],
+    age: Number,
+    idate: Date,
+    isSuccessful: Boolean
+});
+
+const Students = mongoose.model('student', studentSchema);
+
+router.get('/', async(req, res) => {
+    const students = await Students.find();
+    res.send(students);
+});
+
+module.exports = router;
