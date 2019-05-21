c2aca6879f122cb40c2727564369b3ba502b2a9f
Ali Atakan <ali@cimri.com>
2019-05-21 22:47:29 +0100

    Add Delete student with the given ID

--- routes/students.js
+++ routes/students.js
@@ -22,4 +22,15 @@ router.get('/', async(req, res) => {
     res.send(students);
 });
 
-module.exports = router;
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id;
+
+    if(isNaN(id * 0)) return res.status(400).send('Id must be number');
+
+    const student = await Students.findOneAndDelete( { id: parseInt(id) });
+    if (!student) return res.status(404).send('The student with the given ID was not found.');
+  
+    res.send(student);
+})
+
+module.exports = router; 
