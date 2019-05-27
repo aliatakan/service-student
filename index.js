e1255526ebfe13920c990ba2b1fe98983129e72b
Ali Atakan <ali@cimri.com>
2019-05-27 11:55:55 +0100

    Fix winston

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use('/api/students', students);
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
